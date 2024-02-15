f8f8d2075e3d28ed8e746f7ad0d7c952abaaa8cb
Denis <103854079+tkachukds@users.noreply.github.com>
2024-02-15 17:42:49 +0300

    Add files via upload

---
+++ clientSHIFR.py
@@ -0,0 +1,52 @@
+import socket
+
+def encrypt(message, key):
+    encrypted_message = b""
+    for char in message:
+        encrypted_char = (char + key) % 256
+        encrypted_message += bytes([encrypted_char])
+    return encrypted_message
+
+def client():
+    # Создаем сокет
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Запрашиваем у пользователя адрес сервера
+    server_ip = input("Введите IP адрес сервера: ")
+    server_port = 27080  # Порт сервера
+    
+    # Подключаемся к серверу
+    server_address = (server_ip, server_port)
+    
+    try:
+        client_socket.connect(server_address)
+        print(f"Успешное подключение к серверу {server_ip}:{server_port}")
+    except ConnectionRefusedError:
+        print("Не удалось подключиться к серверу. Проверьте правильность IP адреса и порта.")
+        return
+    
+    # Запрашиваем код шифровки у пользователя
+    encryption_key = int(input("Введите трехзначный код шифровки: "))
+
+    while True:
+        # Вводим сообщение от пользователя
+        message = input("Введите сообщение: ")
+        
+        # Шифруем сообщение перед отправкой
+        encrypted_message = encrypt(message.encode('utf-8'), encryption_key)
+        client_socket.send(encrypted_message)
+
+        print(f"Отправлено зашифрованное сообщение: {encrypted_message}")
+        
+        # Получаем ответ от сервера (зашифрованный)
+        encrypted_response = client_socket.recv(1024)
+
+        # Расшифровываем полученный ответ
+        decrypted_response = encrypt(encrypted_response, -encryption_key)
+        print("Получен расшифрованный ответ от сервера:", decrypted_response.decode('utf-8'))
+
+    # Закрываем соединение
+    client_socket.close()
+
+if __name__ == "__main__":
+    client()
---
+++ serverSHIFR_for_py3.py
@@ -0,0 +1,57 @@
+import socket
+
+def encrypt(message, key):
+    encrypted_message = b""
+    for char in message:
+        encrypted_char = (char + key) % 256
+        encrypted_message += bytes([encrypted_char])
+    return encrypted_message
+
+# Получаем IP-адрес текущего хоста
+host_ip = socket.gethostbyname(socket.gethostname())
+
+# Создаем сокет
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Указываем адрес и порт для прослушивания
+host = '0.0.0.0'  # Все доступные интерфейсы
+port = 27080
+
+# Привязываем сокет к адресу и порту
+server_socket.bind((host, port))
+
+# Начинаем прослушивание (максимальное количество подключений - 1)
+server_socket.listen(1)
+
+print("Сервер запущен и прослушивает порт {0}".format(port))
+print("IP-адрес сервера: {0}".format(host_ip))
+
+while True:
+    # Принимаем входящее подключение
+    client_socket, client_address = server_socket.accept()
+
+    print("Установлено соединение с {0}".format(client_address))
+
+    # Запрашиваем код шифровки у клиента
+    encryption_key = int(input("Введите трехзначный код шифровки: "))
+
+    while True:
+        # Читаем данные из сокета
+        data = client_socket.recv(1024)
+
+        if not data:
+            print("Соединение с {0} разорвано".format(client_address))
+            break
+
+        # Расшифровываем полученное сообщение
+        decrypted_data = encrypt(data, -encryption_key)
+
+        print("Получено зашифрованное сообщение от {0}: {1}".format(client_address, decrypted_data.decode('utf-8')))
+
+        # Отправляем ответ клиенту (зашифрованный)
+        response = "Вы писали зашифрованное сообщение: {0}".format(decrypted_data.decode('utf-8'))
+        encrypted_response = encrypt(response.encode('utf-8'), encryption_key)
+        client_socket.send(encrypted_response)
+
+    # Закрываем соединение с клиентом
+    client_socket.close()
